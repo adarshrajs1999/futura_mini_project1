1d40e316c6b59bdd46bf43824952da8fe05a79ef
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-03-23 20:59:16 +0530

    fourth commit

--- myapp/models.py
+++ myapp/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Todo(models.Model):
         subject=models.CharField(max_length=50)
-        data=models.CharField(max_length=100)
+        data=models.TextField()
         date=models.DateField()
